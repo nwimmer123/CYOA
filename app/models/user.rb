5bcfb31b81a14b31c9b70317dff4570402a55567
Noah Wimmer <nwimmer123@yahoo.com>
2016-03-19 16:17:47 -0700

    imported user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  has_many :games
+
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user.try(:authenticate, params[:password])
+  end
+
+  def is_authorized?
+    current_user == @user
+  end
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates_uniqueness_of :email
+  validates :email, format: {with: /@/}
+
 end

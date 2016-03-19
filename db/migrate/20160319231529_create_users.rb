cede72b2c1d269b909dd7ca07740497d0cbc2c6c
Noah Wimmer <nwimmer123@yahoo.com>
2016-03-19 16:18:57 -0700

    inported user migration

--- db/migrate/20160319231529_create_users.rb
+++ db/migrate/20160319231529_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.text :name
+      t.string :email
+      t.string :password_digest
 
       t.timestamps null: false
     end

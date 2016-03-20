419c987ef975031707f07077f3d3a2c46415bd30
Noah Wimmer <nwimmer123@yahoo.com>
2016-03-19 20:52:04 -0700

    generated games controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319231529) do
+ActiveRecord::Schema.define(version: 20160320034729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.text     "title"
+    t.integer  "starting_choice_id"
+    t.boolean  "published"
+    t.text     "backstory"
+    t.integer  "user_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.text     "name"
